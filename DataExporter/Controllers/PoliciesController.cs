a5414854df5d9d3a06048e04231d53b90a035d8d
Stepan Tikhanau <Stepan.Tikhanau@acaciumgroup.com>
2024-05-13 09:32:00 +0200

    Initial commit

---
+++ DataExporter/Controllers/PoliciesController.cs
@@ -0,0 +1,44 @@
+﻿using DataExporter.Dtos;
+using DataExporter.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataExporter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PoliciesController : ControllerBase
+    {
+        private PolicyService _policyService;
+
+        public PoliciesController(PolicyService policyService) 
+        { 
+            _policyService = policyService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostPolicies([FromBody]CreatePolicyDto createPolicyDto)
+        {         
+            return Ok();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetPolicies()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{policyId}")]
+        public async Task<IActionResult> GetPolicy(int id)
+        {
+            return Ok(_policyService.ReadPolicyAsync(id));
+        }
+
+
+        [HttpPost("export")]
+        public async Task<IActionResult> ExportData([FromQuery]DateTime startDate, [FromQuery] DateTime endDate)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ DataExporter/Services/PolicyService.cs
@@ -0,0 +1,61 @@
+﻿using DataExporter.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DataExporter.Services
+{
+    public class PolicyService
+    {
+        private ExporterDbContext _dbContext;
+
+        public PolicyService(ExporterDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbContext.Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Creates a new policy from the DTO.
+        /// </summary>
+        /// <param name="policy"></param>
+        /// <returns>Returns a ReadPolicyDto representing the new policy, if succeded. Returns null, otherwise.</returns>
+        public async Task<ReadPolicyDto?> CreatePolicyAsync(CreatePolicyDto createPolicyDto)
+        {
+            return await Task.FromResult(new ReadPolicyDto());
+        }
+
+        /// <summary>
+        /// Retrives all policies.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Returns a list of ReadPoliciesDto.</returns>
+        public async Task<IList<ReadPolicyDto>> ReadPoliciesAsync()
+        {
+            return await Task.FromResult(new List<ReadPolicyDto>());
+        }
+
+        /// <summary>
+        /// Retrieves a policy by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Returns a ReadPolicyDto.</returns>
+        public async Task<ReadPolicyDto?> ReadPolicyAsync(int id)
+        {
+            var policy = await _dbContext.Policies.SingleAsync(x => x.Id == id);
+            if (policy == null)
+            {
+                return null;
+            }
+
+            var policyDto = new ReadPolicyDto()
+            {
+                Id = policy.Id,
+                PolicyNumber = policy.PolicyNumber,
+                Premium = policy.Premium,
+                StartDate = policy.StartDate
+            };
+
+            return policyDto;
+        }
+    }
+}
